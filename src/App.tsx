a4c097f1e56b1829459c835bc2c255132345124b
Naveen Kosgi <naveenkosigi@gmail.com>
2023-10-21 17:54:30 +0530

    Adds main section layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css';
-import Header from './UI/Layout/Header/Header';
+import Header from "./UI/Layout/Header/Header";
+import Paper from "@mui/material/Paper";
+import classes from './App.module.scss'
 
 function App() {
   return (
     <>
-    <Header/>
+      <Header />
+      <main className={classes['main-container']}>
+        <Paper elevation={3} square sx={{padding:'3rem',position:'relative',borderRadius:'2rem',top:'-1rem'}}>
+          Test Content
+        </Paper>
+      </main>
     </>
   );
 }
--- src/UI/Layout/Header/Header.tsx
+++ src/UI/Layout/Header/Header.tsx
@@ -1,12 +1,11 @@
 import { Typography } from "@mui/material";
 import classes from "./Header.module.scss";
-import logo from '../../../../public/images/nirmata-logo.svg'
 
 const Header = () => {
   return (
     <header className={classes["header-container"]}>
       <div className={classes["logo-container"]}>
-        <div><img src="/images/nirmata-logo.svg"></img></div>
+        <div style={{height:'6rem'}}><img src="/images/nirmata-logo.svg"></img></div>
       </div>
       <nav className={classes["navigation-container"]}>
         <Typography fontWeight={"bolder"} className={classes['nav-item']}>CRICKET</Typography>
